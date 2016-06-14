e87124fa0f6555e7895961322b200f9819ec486d
David Milmont <davidmilmont@gmail.com>
2016-06-14 13:44:18 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/* Configuration Variables
+    confg.username: Your Web Services Username. This can be found in the Admin Console
+    config.secret: Your Web Services Secret. This can also be found in the Admin Console
+    config.reportSuite: The report suite you want to call the data from
+    config.endpoint: The endpoint you will be hitting
+*/
+
+var config = {
+    username:     "user",
+    secret:       "secret",
+    endpoint:     "api.omniture.com"
+};
